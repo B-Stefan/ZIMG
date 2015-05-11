27370ad41a1bb0fc4949fa28def97c2ff4de274a
okeschwien <okeschwien@gmail.com>
2015-05-11 12:09:33 +0200

    created VIEWs and updated ER-Diagram

---
+++ documents/ZIMG_CREATE_VIEWS.sql
@@ -0,0 +1,33 @@
+CREATE VIEW last_upvotes AS
+	SELECT images.fileName, users.name, upvotes.createdAt
+	FROM images, users, upvotes
+	WHERE images.id = upvotes.imageId AND users.id = upvotes.userId AND upvotes.createdAt > (NOW() - INTERVAL 10 DAY);
+
+CREATE VIEW top_ten_tags AS
+	SELECT COUNT(*) AS tag2image, tags.tag, tags.createdAt
+	FROM tag2image, tags
+	WHERE tags.id = tag2image.tagId
+	GROUP BY tag2image.tagId
+	ORDER BY COUNT(*) DESC
+	LIMIT 10;
+
+CREATE VIEW top_ten_images AS
+	SELECT COUNT(*) AS favorite, images.fileName, images.createdAt
+	FROM images, favorite
+	WHERE images.id = favorite.imageId
+	GROUP BY favorite.imageId
+	ORDER BY COUNT(*) DESC
+	LIMIT 10;
+
+CREATE VIEW admins AS
+    SELECT users.id, users.name, users.email, users.password, users.createdAt
+    FROM users
+    WHERE admin = 1;
+
+CREATE VIEW top_uploader AS
+	SELECT COUNT(*) AS images, users.name
+	FROM images, users
+	WHERE images.uploaderId = users.id
+	GROUP BY users.id
+	ORDER BY COUNT(*) DESC
+	LIMIT 5;
