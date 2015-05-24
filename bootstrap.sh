b59fe685649f51fcc735c29e06867f90e172243c
Fabian Junge <fjunge@fabimon.org>
2015-05-24 17:41:53 +0200

    - remove foreign_key_checks in the INSERT file. - change all schema to lowercase in the INSERT file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
-sudo apt-get update
-sudo apt-get -y install mysql-server-5.5
-
 if [ ! -f /var/log/databasesetup ];
 then
+    sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+    sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+    sudo apt-get update
+    sudo apt-get -y install mysql-server-5.5
+
+    echo "Create a User (dbUser) with the Password (dbPass)"
 
     echo "CREATE USER 'dbUser'@'localhost' IDENTIFIED BY 'dbPass'" | mysql -uroot -prootpass
     echo "CREATE DATABASE zimg" | mysql -uroot -prootpass
@@ -16,9 +17,18 @@ then
     touch /var/log/databasesetup
 
     #Create tables
+    echo "Create the Database zimg from File /vagrant/documents/ZIMG_CREATE_TABLE.sql"
     mysql -udbUser -pdbPass zimg < /vagrant/documents/ZIMG_CREATE_TABLE.sql
 
     #Insert default data
+    echo "Init the Database zimg from File /vagrant/documents/ZIMG_INSERT_DATA.sql"
+    mysql -udbUser -pdbPass zimg < /vagrant/documents/ZIMG_INSERT_DATA.sql
+else
+    echo "Recreate the Database zimg from File /vagrant/documents/ZIMG_CREATE_TABLE.sql"
+    mysql -udbUser -pdbPass zimg < /vagrant/documents/ZIMG_CREATE_TABLE.sql
+
+    #Insert default data
+    echo "Init the Database zimg from File /vagrant/documents/ZIMG_INSERT_DATA.sql"
     mysql -udbUser -pdbPass zimg < /vagrant/documents/ZIMG_INSERT_DATA.sql
 fi
 
