1978270ce7f84a6a343b732c903a6884a51e63c1
Stefan B <Stefan.bie@live.de>
2015-08-08 13:24:28 +0200

    - catch all exceptions fix

--- ZIMG-Client/src/main/java/ZIMG/client/controller/BaseController.java
+++ ZIMG-Client/src/main/java/ZIMG/client/controller/BaseController.java
@@ -8,7 +8,18 @@ import org.springframework.web.servlet.ModelAndView;
 public abstract class BaseController {
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleAllException(SpringRuntimeExceptionForUser ex) {
+    public ModelAndView handleAllException(Exception ex) {
+
+        ModelAndView model = new ModelAndView(SpringRuntimeExceptionForUser.DEFAULT_JSP_NAME);
+        model.addObject("errType", SpringRuntimeExceptionForUser.TYPE.ERROR);
+        model.addObject("errMsg", ex.getMessage());
+
+        return model;
+
+    }
+
+    @ExceptionHandler(SpringRuntimeExceptionForUser.class)
+    public ModelAndView handleAllOwn(SpringRuntimeExceptionForUser ex) {
 
         ModelAndView model = new ModelAndView(ex.getJspPageName());
         model.addObject("errType", ex.getType());
